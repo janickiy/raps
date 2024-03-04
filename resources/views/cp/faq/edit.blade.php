ca23f7f9666690f971fc9167f8931249c14f4607
Яницкий Александр Анатольевич <a.yanitskiy@els24.com>
2024-03-04 04:35:27 +0300

    css

---
+++ resources/views/cp/faq/edit.blade.php
@@ -0,0 +1,124 @@
+@extends('cp.app')
+
+@section('title', $title)
+
+@section('css')
+
+
+@endsection
+
+@section('content')
+
+    <!-- START ROW -->
+    <div class="row">
+
+        <!-- NEW COL START -->
+        <article class="col-sm-12 col-md-12 col-lg-12">
+
+            <!-- Widget ID (each widget will need unique ID)-->
+            <div class="jarviswidget" id="wid-id-0" data-widget-colorbutton="false" data-widget-editbutton="false"
+                 data-widget-custombutton="false">
+                <!-- widget options:
+                usage: <div class="jarviswidget" id="wid-id-0" data-widget-editbutton="false">
+                data-widget-colorbutton="false"
+                data-widget-editbutton="false"
+                data-widget-togglebutton="false"
+                data-widget-deletebutton="false"
+                data-widget-fullscreenbutton="false"
+                data-widget-custombutton="false"
+                data-widget-collapsed="true"
+                data-widget-sortable="false"
+                -->
+
+                <!-- widget div-->
+                <div>
+
+                    <!-- widget edit box -->
+                    <div class="jarviswidget-editbox">
+                        <!-- This area used as dropdown edit box -->
+
+                    </div>
+                    <!-- end widget edit box -->
+
+                    <!-- widget content -->
+                    <div class="widget-body no-padding">
+
+                        {!! Form::open(['url' => isset($row) ? URL::route('cp.faq.update') : URL::route('cp.faq.store'), 'method' => isset($row) ? 'put' : 'post', 'class' => "smart-form"]) !!}
+
+                        {!! isset($row) ? Form::hidden('id', $row->id) : '' !!}
+
+                        <header>
+                            *-обязательные поля
+                        </header>
+
+                        <fieldset>
+
+                            <section>
+
+                                {!! Form::label('question', 'Вопрос*', ['class' => 'label']) !!}
+
+                                <label class="input">
+
+                                    {!! Form::text('question', old('question', isset($row) ? $row->question : null), ['class' => 'form-control']) !!}
+
+                                </label>
+
+                                @if ($errors->has('question'))
+                                    <p class="text-danger">{{ $errors->first('question') }}</p>
+                                @endif
+
+                            </section>
+
+                            <section>
+
+                                {!! Form::label('answer', 'Ответ*', ['class' => 'label']) !!}
+
+                                <label class="textarea textarea-resizable">
+
+                                    {!! Form::textarea('answer', old('answer', isset($row) ? $row->answer : null), ['rows' => "3", 'class' => 'custom-scroll']) !!}
+
+                                </label>
+
+                                @if ($errors->has('answer'))
+                                    <p class="text-danger">{{ $errors->first('answer') }}</p>
+                                @endif
+
+                            </section>
+
+                        </fieldset>
+
+                        <footer>
+                            <button type="submit" class="btn btn-primary button-apply">
+                                {{ isset($row) ? 'Изменить' : 'Добавить' }}
+                            </button>
+                            <a class="btn btn-default" href="{{ URL::route('cp.faq.index') }}">
+                                Назад
+                            </a>
+                        </footer>
+
+                        {!! Form::close() !!}
+
+                    </div>
+                    <!-- end widget content -->
+
+                </div>
+                <!-- end widget div -->
+
+            </div>
+            <!-- end widget -->
+
+        </article>
+        <!-- END COL -->
+
+    </div>
+
+    <!-- END ROW -->
+
+@endsection
+
+@section('js')
+
+
+
+
+@endsection
