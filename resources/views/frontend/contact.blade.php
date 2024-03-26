45d738f34b211dc865a07c6dbd231755dd275fae
aleksandranickij <janickiy@mail.ru>
2024-03-26 11:58:13 +0300

    карактеровка кода в разделе контакты

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -29,7 +29,9 @@
             </div>
             <div class="contacts__item">
                 <dt>Телефон</dt>
-                <dd><a href="tel:{{ SettingsHelper::getSetting('PHONE') }}">{{ SettingsHelper::getSetting('PHONE') }}</a></dd>
+                <dd>
+                    <a href="tel:{{ SettingsHelper::getSetting('PHONE') }}">{{ SettingsHelper::getSetting('PHONE') }}</a>
+                </dd>
             </div>
             <div class="contacts__item">
                 <dt>E-mail</dt>
@@ -53,10 +55,9 @@
                 <h2>Карта</h2>
             </div>
             <iframe
-                title="Карта с местоположением офиса RAPSystem"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2997.5346202561846!2d69.21327497624732!3d41.297231901582144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8bca872d740f%3A0xe23a9be4023e2841!2zMTc4INGD0LvQuNGG0LAg0JzRg9C60LjQvNC4LCDQotCw0YjQutC10L3Rgiwg0KPQt9Cx0LXQutC40YHRgtCw0L0!5e0!3m2!1sru!2sru!4v1701696828556!5m2!1sru!2sru"
-                style="border:0;" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-            </iframe>
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1497.0227790972829!2d69.2416803!3d41.3730949!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8da28d84b6ed%3A0xe50e2b8ce7d5bb53!2z0J7QntCeIMKrUkFQU1lTVEVNwrs!5e0!3m2!1sru!2sru!4v1711363938521!5m2!1sru!2sru"
+                width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </section>
 
