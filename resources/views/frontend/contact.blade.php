68aa67d300d00fe3ac399ba7541e8a89d3595241
Яницкий Александр Анатольевич <a.yanitskiy@els24.com>
2024-03-28 01:48:15 +0300

    bugfix

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -34,6 +34,10 @@
                 </dd>
             </div>
             <div class="contacts__item">
+                <dt>GPS координаты</dt>
+                <dd>{{ SettingsHelper::getSetting('GPS') }}</dd>
+            </div>
+            <div class="contacts__item">
                 <dt>E-mail</dt>
                 <dd>{{ SettingsHelper::getSetting('EMAIL') }}</dd>
             </div>
@@ -54,10 +58,7 @@
             <div class="sr-only">
                 <h2>Карта</h2>
             </div>
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1497.0227790972829!2d69.2416803!3d41.3730949!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8da28d84b6ed%3A0xe50e2b8ce7d5bb53!2z0J7QntCeIMKrUkFQU1lTVEVNwrs!5e0!3m2!1sru!2sru!4v1711363938521!5m2!1sru!2sru"
-                width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
+            {!! SettingsHelper::getSetting('MAPS_CODE') !!}
         </div>
     </section>
 
