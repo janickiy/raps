cf325d931a5625cde69c03edc072d21a632b974d
Яницкий Александр Анатольевич <a.yanitskiy@els24.com>
2024-06-29 03:25:57 +0300

    bugfix

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -27,8 +27,7 @@
 
             @if (Auth::check())
                 <p>
-                <a href="{{ URL::route('cp.settings.edit', ['id' =>  SettingsHelper::getId('BLOCK_ABOUT')]) }}" class="editbutton">
-                    Редактировать</a>
+                <a href="{{ URL::route('cp.settings.edit', ['id' =>  SettingsHelper::getId('BLOCK_ABOUT')]) }}" class="editbutton">Редактировать</a>
                 </p>
             @endif
         </div>
@@ -37,8 +36,7 @@
 
         @if (Auth::check())
             <p>
-                <a href="{{ URL::route('cp.settings.edit', ['id' =>  SettingsHelper::getId('BLOCK_ABOUT_2')]) }}" class="editbutton">
-                    Редактировать</a>
+                <a href="{{ URL::route('cp.settings.edit', ['id' =>  SettingsHelper::getId('BLOCK_ABOUT_2')]) }}" class="editbutton">Редактировать</a>
             </p>
         @endif
 
@@ -53,46 +51,22 @@
 
                 @if (Auth::check())
                     <br>
-                    <a href="{{ URL::route('cp.pages.edit', ['id' => $page->id]) }}" class="editbutton">
-                        Редактировать</a>
+                    <a href="{{ URL::route('cp.pages.edit', ['id' => $page->id]) }}" class="editbutton">Редактировать</a>
                 @endif
 
             </div>
-            <div class="about-info__images">
 
-                {!! SettingsHelper::getSetting('BLOCK_ABOUT_IMAGES') !!}
-
-                @if (Auth::check())
-                    <p>
-                        <a href="{{ URL::route('cp.settings.edit', ['id' =>  SettingsHelper::getId('BLOCK_ABOUT_IMAGES')]) }}" class="editbutton">
-                            Редактировать</a>
-                    </p>
-                @endif
-            </div>
-
-            <a href="{{ URL::route('frontend.application') }}" class="btn btn-primary about-info__btn">Оформить
-                заявку</a>
+            <a href="{{ URL::route('frontend.application') }}" class="btn btn-primary about-info__btn">Оформить заявку</a>
 
         </div>
         <div class="about-info__images">
-            <picture class="about-info__img _two-columns">
-                <source srcset="{{ url('/images/hero-img.webp') }}, {{ url('/images/hero-img@2x.webp') }} 2x"
-                        type="image/webp">
-                <img src="{{ url('/images/hero-img.png') }}" srcset="{{ url('/images/hero-img@2x.png') }} 2x" alt="Raps"
-                     loading="lazy">
-            </picture>
-            <picture class="about-info__img">
-                <source srcset="{{ url('/images/hero-img.webp') }}, {{ url('/images/hero-img@2x.webp') }} 2x"
-                        type="image/webp">
-                <img src="{{ url('/images/hero-img.png') }}" srcset="{{ url('/images/hero-img@2x.png') }} 2x" alt="Raps"
-                     loading="lazy">
-            </picture>
-            <picture class="about-info__img">
-                <source srcset="{{ url('/images/hero-img.webp') }}, {{ url('/images/hero-img@2x.webp') }} 2x"
-                        type="image/webp">
-                <img src="{{ url('/images/hero-img.png') }}" srcset="{{ url('/images/hero-img@2x.png') }} 2x" alt="Raps"
-                     loading="lazy">
-            </picture>
+            {!! SettingsHelper::getSetting('BLOCK_ABOUT_IMAGES') !!}
+
+            @if (Auth::check())
+                <p>
+                    <a href="{{ URL::route('cp.settings.edit', ['id' =>  SettingsHelper::getId('BLOCK_ABOUT_IMAGES')]) }}" class="editbutton">Редактировать</a>
+                </p>
+            @endif
         </div>
     </section>
 
